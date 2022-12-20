a04930971d00a5996c4faf816f5864544d173a91
rarepepi <pepimartinez@protonmail.com>
2022-12-20 18:15:26 -0500

    fixes

--- src/partials/Footer.jsx
+++ src/partials/Footer.jsx
@@ -1,33 +1,60 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Footer() {
   return (
     <footer>
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
-
         {/* Top area: Blocks */}
         <div className="grid sm:grid-cols-12 gap-8 py-8 md:py-12 border-t border-gray-200">
-
           {/* 1st block */}
           <div className="sm:col-span-12 lg:col-span-3">
             <div className="mb-2">
               {/* Logo */}
-              <Link to="/" className="inline-block" aria-label="Cruip">
-                <svg className="w-8 h-8" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+              <Link to="/" className="inline-block" aria-label="Summify">
+                <svg
+                  className="w-8 h-8"
+                  viewBox="0 0 32 32"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
                   <defs>
-                    <radialGradient cx="21.152%" cy="86.063%" fx="21.152%" fy="86.063%" r="79.941%" id="footer-logo">
+                    <radialGradient
+                      cx="21.152%"
+                      cy="86.063%"
+                      fx="21.152%"
+                      fy="86.063%"
+                      r="79.941%"
+                      id="footer-logo"
+                    >
                       <stop stopColor="#4FD1C5" offset="0%" />
                       <stop stopColor="#81E6D9" offset="25.871%" />
                       <stop stopColor="#338CF5" offset="100%" />
                     </radialGradient>
                   </defs>
-                  <rect width="32" height="32" rx="16" fill="url(#footer-logo)" fillRule="nonzero" />
+                  <rect
+                    width="32"
+                    height="32"
+                    rx="16"
+                    fill="url(#footer-logo)"
+                    fillRule="nonzero"
+                  />
                 </svg>
               </Link>
             </div>
             <div className="text-sm text-gray-600">
-              <Link to="#" className="text-gray-600 hover:text-gray-900 hover:underline transition duration-150 ease-in-out">Terms</Link> · <Link to="#" className="text-gray-600 hover:text-gray-900 hover:underline transition duration-150 ease-in-out">Privacy Policy</Link>
+              <Link
+                to="#"
+                className="text-gray-600 hover:text-gray-900 hover:underline transition duration-150 ease-in-out"
+              >
+                Terms
+              </Link>{" "}
+              ·{" "}
+              <Link
+                to="#"
+                className="text-gray-600 hover:text-gray-900 hover:underline transition duration-150 ease-in-out"
+              >
+                Privacy Policy
+              </Link>
             </div>
           </div>
 
@@ -119,14 +146,13 @@ function Footer() {
               </div>
             </form>
           </div>           */}
-
         </div>
 
         {/* Bottom area */}
         {/* <div className="md:flex md:items-center md:justify-between py-4 md:py-8 border-t border-gray-200"> */}
 
-          {/* Social links */}
-          {/* <ul className="flex mb-4 md:order-1 md:ml-4 md:mb-0">
+        {/* Social links */}
+        {/* <ul className="flex mb-4 md:order-1 md:ml-4 md:mb-0">
             <li>
               <Link to="#" className="flex justify-center items-center text-gray-600 hover:text-gray-900 bg-white hover:bg-white-100 rounded-full shadow transition duration-150 ease-in-out" aria-label="Twitter">
                 <svg className="w-8 h-8 fill-current" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
@@ -150,11 +176,10 @@ function Footer() {
             </li>
           </ul> */}
 
-          {/* Copyrights note */}
-          {/* <div className="text-sm text-gray-600 mr-4">Made by <a className="text-blue-600 hover:underline" href="https://cruip.com/">Cruip</a>. All rights reserved.</div> */}
+        {/* Copyrights note */}
+        {/* <div className="text-sm text-gray-600 mr-4">Made by <a className="text-blue-600 hover:underline" href="https://cruip.com/">Cruip</a>. All rights reserved.</div> */}
 
         {/* </div> */}
-
       </div>
     </footer>
   );
